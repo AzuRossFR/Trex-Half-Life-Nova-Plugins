fd163bb2af5b4641c8fd8e85369dce52794e885d
AzuRoss <32108173+AzuRossFR@users.noreply.github.com>
2024-02-19 00:31:39 +0100

    `résine du cartel`

--- tenues/items/sh_resin.lua
+++ tenues/items/sh_resin.lua
@@ -1,7 +1,7 @@
 
 ITEM.name = "Résine du Cartel"
 ITEM.description = [[La résine est une forme avancée de matériau de construction.
-On la trouve sous forme de "palets" et elle est utilisée dans le système de construction des armes du Cartel..
+On la trouve sous forme de "palets" et elle est utilisée dans le système de construction des armes du Cartel...
 
 ]]
 ITEM.rarity = 5
