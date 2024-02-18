388f5565bde0206b9036fddad72807b073c69481
AzuRoss <32108173+AzuRossFR@users.noreply.github.com>
2024-02-19 00:30:49 +0100

    - Création des items suivant :

---
+++ tenues/items/sh_resin.lua
@@ -0,0 +1,11 @@
+
+ITEM.name = "Résine du Cartel"
+ITEM.description = [[La résine est une forme avancée de matériau de construction.
+On la trouve sous forme de "palets" et elle est utilisée dans le système de construction des armes du Cartel..
+
+]]
+ITEM.rarity = 5
+ITEM.rarityname = "Objet du Cartel"
+ITEM.model = Model("models/gibs/metal_gib4.mdl")
+ITEM.width = 1
+ITEM.height = 1
